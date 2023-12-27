07131ebe834a4e6f2a3b5d3b7258b3bd7eb03785
marc <marchajjar05@gmail.com>
2023-12-27 15:09:29 +0200

    Add new chat functionality

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,14 +1,24 @@
 import React from "react";
 import './SidebarChat.css'
 import { Avatar } from "@mui/material";
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from "./firebase";
 
 function SidebarChat({ id, name, addNewChat }) {
 
-  const createChat = () => {
-    const roomName = prompt("Please enter name for chat");
+  const createChat = async () => {
+    const roomName = prompt("Please enter name for chat room");
 
     if (roomName) {
-      // do some database stuff...
+      try {
+        const docRef = await addDoc(collection(db, 'rooms'), {
+          name: roomName,
+        });
+
+        console.log("document writtne with ID: ", docRef.id);
+      } catch (e) {
+        console.log("Error adding document: ", e);
+      }
     }
   }
 
