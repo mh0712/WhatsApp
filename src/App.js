db31aca55cf8bb5cd7852855852f25e99d5d925b
NaddafCeline <celine.naddaf@net.usj.edu.lb>
2023-12-28 18:26:18 +0200

    adding the status page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
+import React from 'react';
 import './assets/styles/App.css';
-import Sidebar from "./components/SideBar/Sidebar.js"
-import Chat from './components/Chat/Chat.js';
 
+import ChattingPage from './Pages/ChattingPage';
+import StatusPage from './Pages/StatusPage';
 function App() {
-  return (
-    <div className="app">
-      <div className='app_body'>
-        <Sidebar/>
-        <Chat />
-      </div>
-    </div>
-  );
+  return <StatusPage/>
 }
 
 export default App;
