33d1fb240fbf0108747ba301a21a09bc385a0f03
NaddafCeline <celine.naddaf@net.usj.edu.lb>
2024-01-08 18:17:16 +0200

    tail of messages

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -123,11 +123,15 @@ function Chat({ lastseen, selectedChat }) {
         </div>
       </div>
 
-      <div className="chat_body">
-        {messages.map((message) => (
+          <div className="chat_body">
+        {messages.map((message, i) => (
           <p
             key={message.id}
-            className={`chat_message ${message.sender === auth.currentUser.uid ? "chat_sender" : "chat_receiver"}`}
+            className={`chat_message ${
+              message.sender === auth.currentUser.uid
+                ? "chat_sender"
+                : "chat_receiver"
+            } ${i < messages.length - 1 && messages[i + 1]?.sender === message.sender ? "noTail" : ""}`}
           >
             {message.content}
             <span className="chat_timestamp">{message.timestamp}</span>
